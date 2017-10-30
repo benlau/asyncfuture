153ae6a3ccaf4200f78270da164572bafc1e746f
Ben Lau <xbenlau@gmail.com>
2017-10-31 02:32:28 +0800

    compilererrors: Add few examples of context()

--- tests/compilererrors/compilererrors.cpp
+++ tests/compilererrors/compilererrors.cpp
@@ -9,9 +9,10 @@ using namespace AsyncFuture;
 
 #define ENABLED
 
+// Case 1 - More than 1 argument in callback function
+
 #if defined(ENABLED)
-// More than 1 argument in callback function
-static void case1a() {
+static void case1a() { // subscribe - no return
     auto defer = Deferred<int>();
 
     observe(defer.future()).subscribe([=](int a, int b) {
@@ -23,7 +24,7 @@ static void case1a() {
 #endif
 
 #if defined(ENABLED)
-static void case1b() {
+static void case1b() {  // subscribe - with return
     auto defer = Deferred<int>();
 
     observe(defer.future()).subscribe([=](int a, int b) {
@@ -35,10 +36,36 @@ static void case1b() {
 #endif
 
 #if defined(ENABLED)
-static void case2() {
+static void case1c() {  // context - with return
+    auto defer = Deferred<int>();
+
+    QObject tmp;
+
+    observe(defer.future()).context(&tmp, [=](int a, int b) {
+        Q_UNUSED(a);
+        Q_UNUSED(b);
+        return 99;
+    });
+}
+#endif
+
+// Case 1 - observe a QFuture<void> by a callback with input argument
+#if defined(ENABLED)
+static void case2a() {
     auto defer = Deferred<void>();
 
     observe(defer.future()).subscribe([=](int a) {
+        Q_UNUSED(a);
+    });
+}
+#endif
+
+#if defined(ENABLED)
+static void case2b() {
+    auto defer = Deferred<void>();
+    QObject tmp;
+
+    observe(defer.future()).context(&tmp, [=](int a) {
         Q_UNUSED(a);
     });
 }
