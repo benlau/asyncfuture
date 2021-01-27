68f1df754cca0c0901cadbc61c70e4f3d4721a15
Ben Lau <xbenlau@gmail.com>
2021-01-28 04:50:52 +0800

    t2

---
+++ .travis/run-tests.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+export DISPLAY=:99.0
+export QT_QPA_PLATFORM=minimal
+
+set -v
+set -e
+
+qpm check
+VERSION=$(jq .version.label qpm.json)
+VERSION=$(eval echo $VERSION)
+ulimit -c unlimited 
+ulimit -a
+if ! head -n 1 asyncfuture.h | grep -q $VERSION; then echo "Version string mismatched"; exit -1 ;fi
+
+pushd tests/asyncfutureunittests/
+qmake asyncfutureunittests.pro
+make
+./asyncfutureunittests
+popd
+
+ls
+if test -f coredump; then gdb -ex "where \n ;  thread apply all bt" asyncfutureunittests coredump  ; fi
+valgrind --num-callers=30 --leak-check=full --track-origins=yes --gen-suppressions=all --error-exitcode=1 --suppressions=../tests/asyncfutureunittests/asyncfuture.supp ./asyncfutureunittests
